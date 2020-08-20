e718ad46ef06a62a80b0944f3591b1db7e8f6295
ericmbouwe <ericmbouwe@yahoo.fr>
2020-08-20 23:37:12 +0100

    style added to menu page

--- src/menu.js
+++ src/menu.js
@@ -1,22 +1,31 @@
 export function createMenuPage() {
   const wrapper = document.createElement('div')
   const card = document.createElement('div')
-  const title = document.createElement('p')
+  const card1 = document.createElement('div')
+  const card2 = document.createElement('div')
 
   wrapper.appendChild(card)
-  wrapper.appendChild(title)
+  wrapper.appendChild(card1)
+  wrapper.appendChild(card2)
 
   wrapper.className = 'menu'
   wrapper.id = 'menuPage'
   wrapper.classList.add('tab')
 
-  card.innerText = `MENU Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repudiandae maiores soluta repellendus obcaecati,
+  card.innerText = `MENU 1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repudiandae maiores soluta repellendus obcaecati,
     quo adipisci perferendis earum iusto assumenda quod ut nostrum non necessitatibus ipsa rerum eligendi! Minus, illum!
     Accusamus non quia libero quasi eveniet omnis soluta.Impedit in iusto maxime odit reiciendis quaerat saepe et
-    veritatis, hic reprehenderit earum sapiente sit amet ea nam quia, tempora aperiam aliquid ?
-      Quod, doloribus eius ipsum inventore, cum exercitationem saepe quam necessitatibus iure maxime unde recusandae,
-        similique praesentium esse ratione aspernatur dignissimos alias.Veniam quos et inventore at amet, architecto dicta
-    tempora`
+    veritatis, hic reprehenderit earum sapiente sit amet ea nam quia, tempora aperiam aliquid`
+
+  card1.innerText = `MENU 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repudiandae maiores soluta repellendus obcaecati,
+    quo adipisci perferendis earum iusto assumenda quod ut nostrum non necessitatibus ipsa rerum eligendi! Minus, illum!
+    Accusamus non quia libero quasi eveniet omnis soluta.Impedit in iusto maxime odit reiciendis quaerat saepe et
+    veritatis, hic reprehenderit earum sapiente sit amet ea nam quia, tempora aperiam aliquid`
+
+  card2.innerText = `MENU 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repudiandae maiores soluta repellendus obcaecati,
+    quo adipisci perferendis earum iusto assumenda quod ut nostrum non necessitatibus ipsa rerum eligendi! Minus, illum!
+    Accusamus non quia libero quasi eveniet omnis soluta.Impedit in iusto maxime odit reiciendis quaerat saepe et
+    veritatis, hic reprehenderit earum sapiente sit amet ea nam quia, tempora aperiam aliquid `
   
   return wrapper
 }
